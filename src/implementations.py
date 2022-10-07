d43f16850193d1845be69d170b51add15f61d3ea
justLars7D1 <Larsquaedvlieg@outlook.com>
2022-10-07 11:08:26 +0200

    Basic changes

---
+++ src/implementations.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    pass
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    pass
+
+
+def least_squares(y, tx):
+    pass
+
+
+def ridge_regression(y, tx, lambda_):
+    pass
+
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    pass
+
+
+def reg_logistic_regression(y: np.array, tx: np.array, lambda_: float, initial_w: np.array, max_iters: int, gamma: float) -> [np.array, float]:
+    pass
