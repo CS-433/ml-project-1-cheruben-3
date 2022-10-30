6369ad055610da1fd2a2fbe2f75f08ad9d30736e
justLars7D1 <Larsquaedvlieg@outlook.com>
2022-10-30 20:36:04 +0100

    Documented my implementation functions

--- src/implementations.py
+++ src/implementations.py
@@ -13,13 +13,20 @@ from metrics import (
 def mean_squared_error_gd(
     y: np.array, tx: np.array, initial_w: np.array, max_iters: int, gamma: float
 ) -> (np.array, float):
-    """
-    :param y:
-    :param tx:
-    :param initial_w:
-    :param max_iters:
-    :param gamma:
-    :return:
+    """Performs gradient descent on a linear regression model to find the optimal parameters and final loss
+
+    Args:
+        y: 2d numpy array where the rows represent a sample the column value represents the label of the sample
+        tx: 2d numpy array where the rows represent a sample the column values represent the features of the sample
+        initial_w: numpy array representing the weights from which to start optimization
+        max_iters: the number of iterations to run gradient descent for
+        gamma: the learning rate
+
+    Returns:
+        2-tuple of the following:
+        - The final weight after max_iters iterations
+        - The loss of the final weights on (tx, y)
+
     """
     w = initial_w
 
@@ -36,13 +43,20 @@ def mean_squared_error_gd(
 def mean_squared_error_sgd(
     y: np.array, tx: np.array, initial_w: np.array, max_iters: int, gamma: float
 ) -> (np.array, float):
-    """
-    :param y:
-    :param tx:
-    :param initial_w:
-    :param max_iters:
-    :param gamma:
-    :return:
+    """Performs STOCHASTIC gradient descent on a linear regression model to find the optimal parameters and final loss
+
+    Args:
+        y: 2d numpy array where the rows represent a sample the column value represents the label of the sample
+        tx: 2d numpy array where the rows represent a sample the column values represent the features of the sample
+        initial_w: numpy array representing the weights from which to start optimization
+        max_iters: the number of iterations to run gradient descent for
+        gamma: the learning rate
+
+    Returns:
+        2-tuple of the following:
+        - The final weight after max_iters iterations
+        - The loss of the final weights on (tx, y)
+
     """
 
     w = initial_w
@@ -59,21 +73,36 @@ def mean_squared_error_sgd(
 
 
 def least_squares(y: np.array, tx: np.array) -> (np.array, float):
-    """
-    :param y:
-    :param tx:
-    :return:
+    """Returns the least-squares approximation of a linear regression model to find the optimal parameters and final loss
+
+    Args:
+        y: 2d numpy array where the rows represent a sample the column value represents the label of the sample
+        tx: 2d numpy array where the rows represent a sample the column values represent the features of the sample
+
+    Returns:
+        2-tuple of the following:
+        - The final weight after max_iters iterations
+        - The loss of the final weights on (tx, y)
+
     """
     w_optim = np.linalg.lstsq(tx.T @ tx, tx.T @ y, rcond=None)[0]
     return w_optim, MSELoss.loss(tx, y, w_optim)
 
 
 def ridge_regression(y: np.array, tx: np.array, lambda_: float) -> (np.array, float):
-    """
-    :param y:
-    :param tx:
-    :param lambda_:
-    :return:
+    """Returns the least-squares approximation of a REGULARIZED linear regression model to find the optimal parameters 
+    and final loss (note that lambda_ could be 0)
+
+    Args:
+        y: 2d numpy array where the rows represent a sample the column value represents the label of the sample
+        tx: 2d numpy array where the rows represent a sample the column values represent the features of the sample
+        lambda_: the weight on the regularization term of the loss (higher = more regularization)
+
+    Returns:
+        2-tuple of the following:
+        - The final weight after max_iters iterations
+        - The loss of the final weights on (tx, y)
+
     """
     regularizer_part = 2 * len(y) * lambda_ * np.eye(tx.shape[1])
     w_optim = np.linalg.lstsq(tx.T @ tx + regularizer_part, tx.T @ y, rcond=None)[0]
@@ -83,13 +112,20 @@ def ridge_regression(y: np.array, tx: np.array, lambda_: float) -> (np.array, fl
 def logistic_regression(
     y: np.array, tx: np.array, initial_w: np.array, max_iters: int, gamma: float
 ) -> (np.array, float):
-    """
-    :param y:
-    :param tx:
-    :param initial_w:
-    :param max_iters:
-    :param gamma:
-    :return:
+    """Performs gradient descent on a logistic regression model to find the optimal parameters and final loss
+
+    Args:
+        y: 2d numpy array where the rows represent a sample the column value represents the label of the sample
+        tx: 2d numpy array where the rows represent a sample the column values represent the features of the sample
+        initial_w: numpy array representing the weights from which to start optimization
+        max_iters: the number of iterations to run gradient descent for
+        gamma: the learning rate
+
+    Returns:
+        2-tuple of the following:
+        - The final weight after max_iters iterations
+        - The loss of the final weights on (tx, y)
+
     """
 
     w = initial_w
@@ -116,14 +152,29 @@ def reg_logistic_regression(
     gamma: float,
     return_all_losses: bool = False,
 ) -> (np.array, float):
-    """
-    :param y:
-    :param tx:
-    :param lambda_:
-    :param initial_w:
-    :param max_iters:
-    :param gamma:
-    :return:
+    """Performs gradient descent on a REGULARIZED logistic regression model to find the optimal parameters and final
+    loss (note that lambda_ could be 0)
+
+    Args:
+        y: 2d numpy array where the rows represent a sample the column value represents the label of the sample
+        tx: 2d numpy array where the rows represent a sample the column values represent the features of the sample
+        lambda_: the weight on the regularization term of the loss (higher = more regularization)
+        initial_w: numpy array representing the weights from which to start optimization
+        max_iters: the number of iterations to run gradient descent for
+        gamma: the learning rate
+        return_all_losses: boolean for the purpose of tracking the history of the loss, useful for experimenting
+
+    Returns:
+        IF return_all_losses is FALSE:
+            2-tuple of the following:
+            - The final weight after max_iters iterations
+            - The loss of the final weights on (tx, y)
+        IF return_all_losses is TRUE:
+            3-tuple of the following:
+            - The final weight after max_iters iterations
+            - The loss of the final weights on (tx, y)
+            - The history of the loss on each iteration
+
     """
     w = initial_w
 
@@ -154,14 +205,32 @@ def reg_logistic_regression_AGDR(
     gamma: float,
     return_all_losses: bool = False,
 ) -> (np.array, float):
-    """
-    :param y:
-    :param tx:
-    :param lambda_:
-    :param initial_w:
-    :param max_iters:
-    :param gamma:
-    :return:
+    """Performs ACCELERATED gradient descent WITH RESTART on a REGULARIZED logistic regression model to find the optimal
+    parameters and final loss (note that lambda_ could be 0)
+
+    -> On a high-level, this gradient descent algorithm keeps some sort of "momentum" to boost the algorithm in the
+    right direction and resets this momentum when it goes in a bad direction
+
+    Args:
+        y: 2d numpy array where the rows represent a sample the column value represents the label of the sample
+        tx: 2d numpy array where the rows represent a sample the column values represent the features of the sample
+        lambda_: the weight on the regularization term of the loss (higher = more regularization)
+        initial_w: numpy array representing the weights from which to start optimization
+        max_iters: the number of iterations to run gradient descent for
+        gamma: the learning rate
+        return_all_losses: boolean for the purpose of tracking the history of the loss, useful for experimenting
+
+    Returns:
+        IF return_all_losses is FALSE:
+            2-tuple of the following:
+            - The final weight after max_iters iterations
+            - The loss of the final weights on (tx, y)
+        IF return_all_losses is TRUE:
+            3-tuple of the following:
+            - The final weight after max_iters iterations
+            - The loss of the final weights on (tx, y)
+            - The history of the loss on each iteration
+
     """
     w = v = initial_w
     t = 1
